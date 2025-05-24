031221e7879b70a4018cb656bbbb702439f7f14a
Aghogho <monorienaghogho@gmail.com>
2025-05-24 23:20:10 +0100

    finished

--- W02/Next-Intro/nextjs-dashboard/app/lib/actions.ts
+++ W02/Next-Intro/nextjs-dashboard/app/lib/actions.ts
@@ -70,7 +70,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
         VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `;
     } catch (error) {
-        // console.error(error);
+        console.error(error);
         return {
             message: 'Database Error: Failed to Create Invoice.',
         };
