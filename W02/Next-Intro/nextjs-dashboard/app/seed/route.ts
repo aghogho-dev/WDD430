a314339bddf0220f2b00aacc6be49ee7921f6f6e
Aghogho <monorienaghogho@gmail.com>
2025-05-24 23:27:24 +0100

    finished

--- W02/Next-Intro/nextjs-dashboard/app/seed/route.ts
+++ W02/Next-Intro/nextjs-dashboard/app/seed/route.ts
@@ -103,12 +103,14 @@ async function seedRevenue() {
 
 export async function GET() {
   try {
-    // const result = await sql.begin((sql) => [
-    //   seedUsers(),
-    //   seedCustomers(),
-    //   seedInvoices(),
-    //   seedRevenue(),
-    // ]);
+    const result = await sql.begin((sql) => [
+      seedUsers(),
+      seedCustomers(),
+      seedInvoices(),
+      seedRevenue(),
+    ]);
+
+    console.log(result);
 
     return Response.json({ message: 'Database seeded successfully' });
   } catch (error) {
